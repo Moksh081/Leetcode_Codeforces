89ceb8bfef9df0afbffa9fe822ab867fa165a7ee
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2024-04-10 17:09:19 +0530

    Update intersection_of_2arr.cpp

--- intersection_of_2arr.cpp
+++ intersection_of_2arr.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         int m = nums2.size();
@@ -14,5 +14,26 @@ public:
             }
         }
         return result;
+    }
+};*/
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ans;
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0;
+        int j = 0;
+        while(i < n && j < m){
+                if(nums1[i]==nums2[j]){
+                    ans.push_back(nums1[i]);
+                    i++;
+                j++;
+            } else if(nums1[i] < nums2[j]) i++;
+            else j++;
+        }
+        return ans;
     }
 };
