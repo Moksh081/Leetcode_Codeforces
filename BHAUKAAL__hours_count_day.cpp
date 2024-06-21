617d6a5c701e00c37edda5ff91ba2098abcbe3e3
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2024-06-21 13:05:03 +0530

    Add files via upload

---
+++ BHAUKAAL__hours_count_day.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCompleteDayPairs(vector<int>& hours) {
+    long long ans = 0;
+    unordered_map<int, int> ump;
+    for(auto h: hours){
+        ans += ump[(24 - h%24)%24];
+        ump[h%24]++;
+    }
+    return ans;
+}
+};
+
