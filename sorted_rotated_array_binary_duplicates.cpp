ff9a06d92583794bd7ad8ef12e43345eea81eabf
Moksh Sharma <109467184+Moksh081@users.noreply.github.com>
2024-07-19 10:09:09 +0530

    Add files via upload

---
+++ sorted_rotated_array_binary_duplicates.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            // The tricky part is handling duplicates.
+            // When we encounter duplicates, we can just move the pointers
+            // closer to each other.
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid]) { // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
+
